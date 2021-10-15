1d0435dc4425a64174cc5e192a86b4633e420223
Shanon <shanon.richet@gmail.com>
2021-10-15 15:15:52 +0200

    1 mole can be visible

--- script.js
+++ script.js
@@ -13,23 +13,23 @@ for (let i= 0; i< 6; i++){
     var hole= document.createElement('p')
     hole.setAttribute('class', 'hole')
     main.appendChild(hole)
-    var taupe= document.createElement('p')
-    taupe.setAttribute('class', 'taupe_cachée')
-    hole.appendChild(taupe)
+    var taupeCache= document.createElement('p')
+    taupeCache.setAttribute('class', 'taupe_cachée')
+    hole.appendChild(taupeCache)
 }
 const holes= document.querySelectorAll('.hole')
 const taupes= document.querySelectorAll('.taupe_cachée')
 
 
-setTimeout(interval, 2000)
+setTimeout(interval, 3000)
 var interval = setInterval(() => {
-    taupes[Math.floor(Math.random()*5)].setAttribute('class', 'taupe_visible')  
-    clearInterval(interval)
+    var taupeVisible= document.querySelector('.taupe_visible')
+    taupes[Math.floor(Math.random()*5)].setAttribute('class', 'taupe_visible') 
+    taupeVisible.removeAttribute('class', 'taupe_visible')
 },1000);
 
-var taupeVisible= document.querySelector('.taupe_visible')
-taupeVisible.onclick= () => {
-    alert('e')
-}
+
+
+
 
 
