16aceca6f86df86893e059d3ff3e9a298245a4e2
shanon-richet <89406450+shanon-richet@users.noreply.github.com>
2022-02-15 21:57:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,10 @@ const main = document.querySelector('main')
 const clock = document.querySelector('.timer')
 const score = document.querySelector('.score')
 const holes = document.querySelectorAll('.hole-container')
-const taupes = document.querySelectorAll('.hiddenMole')
+const taupes = document.querySelectorAll('#hiddenMole')
 const hole = document.querySelector('section')
 const chances = document.querySelectorAll('.chances img')
-const storedScores = localStorage.getItem('score')
-if (storedScores == ! null) {
-    JSON.parse(storedScores)
-    for (const elem of storedScores) {
-        console.log(elem)
-        const scoreHistory = document.createElement('p')
-        scoreHistory.innerHTML = elem.score
-        main.appendChild(scoreHistory)
-    }
-}
+
 document.body.addEventListener('keydown', startEvent)
 function startEvent() {
     var x = event.keyCode;
@@ -23,49 +14,41 @@ function startEvent() {
     }
 }
 
+function tapMole() {
+    alert('r')
+}
 function startGame() {
-    var scoreValue = 1;
-    var chance = 3;
-    var myValue = 0;
+    var scoreValue = 1
+    var chance = 3
+    var myValue = 0
+
     setInterval(() => {
-        randomIndex = Math.floor(Math.random() * 5)
-    }, 2000);
-    //timer
-    setInterval(() => {
-        clock.value = myValue;
-        myValue = myValue + 1;
-        clock.innerHTML = myValue;
-    }, 1000);
-    setInterval(() => {
-        visibleMole = taupes[randomIndex]
-        visibleMole.classList.remove('hiddenMole')
-        visibleMole.classList.add('visibleMole')
-        visibleMole.addEventListener('click', tapMole)
-        const tapMole = () => {
-            alert('e')
-            visibleMole.classList.remove('visibleMole')
+        const randomTaupe = taupes[Math.floor(Math.random() * 5)]
+        randomTaupe.classList.add('visibleMole')
+        randomTaupe.onclick= function tapMole() {
             score.value = scoreValue;
             scoreValue = scoreValue + 1
+            randomTaupe.classList.remove('visibleMole')
         }
         setTimeout(() => {
-            if (visibleMole.classList.contains('visibleMole')) {
-                visibleMole.classList.remove('visibleMole')
-                chances[chance = chance - 1].remove()
-                if (chance === 0) {
-                    localStorage.setItem('score', `"score": "${JSON.stringify(scoreValue)}"`)
-                    alert('You lose !')
-                    scoreValue = 0;
-                    myValue = 0;
-                    chance = 3;
-                    document.location.reload()
-
+            if (randomTaupe.classList.contains('visibleMole')) {
+                    randomTaupe.classList.remove('visibleMole')
+                    chances[chance = chance - 1].remove()
                 }
-            }
-        }, 1800)
-        visibleMole.classList.add('hiddenMole')
-    }, 3000)
+            if (chance === 0) {
+                alert('You lose !')
+                scoreValue = 0
+                myValue = 3
+                document.location.reload()
+            }  
+
+        }, 1900)
+    }, 2000)
+
+    setInterval(() => {
+        clock.value = myValue
+        myValue = myValue + 1
+        clock.innerHTML = myValue
+    }, 1000)
 }
 
-taupes.onclick = () => {
-    alert('o')
-}
